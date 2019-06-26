916a14a9499fc4d0c3a7226abeb12df86379fc31
Aplline <52078464+Aplline@users.noreply.github.com>
2019-06-26 10:07:35 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,53 @@
+'use strict';
+// Module Dependencies
+// -------------------
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var errorhandler = require('errorhandler');
+var http        = require('http');
+var JWT         = require('./lib/jwtDecoder');
+var path        = require('path');
+var request     = require('request');
+var routes      = require('./routes');
+var activity    = require('./routes/activity');
+var trigger     = require('./routes/trigger');
+
+var app = express();
+
+var APIKeys = {
+    clientId        : 'leobro75019',
+    clientSecret    : 'R_ab2b05404c8947f79602f4a21abbfd0a',
+    access_token    : 'd9c0c6bc56a8989813bda6d16bf8a5b35a3fbc0f',
+    authUrl         : 'https://api-ssl.bitly.com/v4/shorten'
+};
+
+
+// Configure Express
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.raw({type: 'application/jwt'}));
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+//app.use(express.methodOverride());
+//app.use(express.favicon());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express in Development Mode
+if ('development' == app.get('env')) {
+  app.use(errorhandler());
+}
+
+// HubExchange Routes
+app.get('/', routes.index );
+app.post('/login', routes.login );
+app.post('/logout', routes.logout );
+
+// Custom Journey Builder Activity Routes
+app.post('/journeybuilder/save/', activity.save );
+app.post('/journeybuilder/validate/', activity.validate );
+app.post('/journeybuilder/publish/', activity.publish );
+app.post('/journeybuilder/execute/', activity.execute );
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
